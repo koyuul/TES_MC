6cce04a9a3e7b23f1a09f3682858df0d3bf138a2
Kyle Deleon <shannonkyle.deleon@gmail.com>
2024-06-22 18:11:34 -0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar/Sidebar"
+import Viewbox from "./Viewbox/Viewbox";
+
+export default () => (
+  <div id="app">
+    <Sidebar />
+    <Viewbox />
+  </div>
+);
---
+++ src/Viewbox/Viewbox.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import "./Viewbox.css"
+
+export default class Viewbox extends Component {
+    render() {
+        return (
+            <div id="viewbox">Viewbox</div>
+        )
+    }
+}
