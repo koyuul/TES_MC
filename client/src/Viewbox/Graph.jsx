1866ac7475727f1fee79106c1288db054fc3d587
TechEdSat <shannonkyle.deleon@gmail.com>
2024-07-22 10:23:33 -0700

    edit request to serve initial data

--- client/src/Viewbox/Graph.jsx
+++ client/src/Viewbox/Graph.jsx
@@ -15,6 +15,7 @@ import io from 'socket.io-client'
 
 ChartJS.register(TimeScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
+
 function Graph(props) {
     const [dataPackets, setDataPackets] = useState([]);
 
@@ -34,19 +35,6 @@ function Graph(props) {
         return () => {
             socket.disconnect();
         };
-        // const fetchData = async () => {
-        //     try {
-        //         const response = await fetch('http://localhost:3000/TES13');
-        //         if (!response.ok) {
-        //             throw new Error('Network response was not ok');
-        //         }
-        //         setDataPackets(await response.json());
-        //     } catch (error) {
-        //         console.error('There has been a problem with your fetch operation:', error);
-        //     }
-        // };
-
-        // fetchData();
     }, []); // Empty dependency array means this effect runs once after initial render
 
     const options = {
--- server/server.js
+++ server/server.js
@@ -15,17 +15,16 @@ let data = [];
 rethinkdb.connect({ host: 'localhost', port: 28015 }, (err, conn) => {
     if (err) throw err;
     console.log('RethinkDB Connected');
-
     rethinkdb.db('TES_13').table('Powerboard')
         .orderBy({ index: rethinkdb.desc('EPOCH') })
-        .limit(10)
+        .limit(20)
         .changes({ includeInitial: true })
         .run(conn, function (err, cursor) {
             cursor.each((err, change) => {
                 if (err) throw err;
                 console.log(`Sending packet from ${change['new_val']['EPOCH']} to websocket`);
-                data.push(change);
-                io.emit('change', change);
+                data.push(change['new_val']);
+                io.emit('change', change['new_val']);
             });
         });
 });
@@ -46,43 +45,3 @@ io.on('connection', (socket) => {
 server.listen(PORT, () => {
     console.log(`TES_MC server listening at http://localhost:${PORT}/`);
 })
-
-// // Allow CORS requests so that we can request data freely
-
-
-// app.get('/TES13', (req, res) => {
-//     rethinkdb.connect({ host: 'localhost', port: 28015 }, (err, conn) => {
-//         if (err) {
-//             res.send(err)
-//             console.log(`No rethinkdb connection at http://localhost:${port}/TES13. Have you started the server?`)
-//             return;
-//         }
-
-//         connection = conn;
-//         console.log('test');
-//         rethinkdb.db('TES_13')
-//             .table('Powerboard')
-//             .orderBy({ index: rethinkdb.desc('EPOCH') })
-//             .limit(10)
-//             .changes({ includeInitial: true })
-//             .run(conn, function (err, cursor) {
-//                 if (err) throw err;
-//                 cursor.each(console.log);
-//                 // let packets = [];
-//                 // cursor.each(function (err, row) {
-//                 //     if (err) throw err;
-//                 //     packets.push(row);
-//                 // });
-//                 // console.log(packets);
-//                 // res.json(packets);
-//             })
-//     });
-// });
-
-
-// app.get('/', (req, res) => {
-//     res.send('success boi')
-// })
-
-// const port = process.env.PORT || 3000;
-// app.listen(3000, () => console.log(`TES_MC Server listening at http://localhost:${port}/`));
